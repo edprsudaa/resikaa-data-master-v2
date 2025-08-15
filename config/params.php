a46aa6f668553ba584a0d5ef785c44f5cb8358aa
edprsudaa <edp.rsudaa@gmail.com>
2025-08-15 14:35:12 +0700

    Penambahan file params.php

---
+++ config/params.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'name-aplikasi' => 'Data Master RSUDAA',
+    'bsVersion' => '4.x', // this will set globally `bsVersion` to Bootstrap 4.x for all Krajee Extensions
+];
